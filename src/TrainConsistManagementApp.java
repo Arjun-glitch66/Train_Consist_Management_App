d1aff6bd1d5cc4fa5a110a16fcd3054f51649f07
Arjun <arjunanand206@gmail.com>
2026-03-27 16:20:55 +0530

    Maintain Ordered Bogie IDs (TreeSet & SortedSet)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -2,18 +2,23 @@ import java.util.*;
 class TrainConsistManagementApp {
     void task() {
         System.out.println("===============================");
-        System.out.println("UC3- Track unique boggie ids");
+        System.out.println("UC4- Maintain Ordered Boggie Consist");
         System.out.println("================================");
-        Set<String> bogies = new HashSet<>();
-        bogies.add("BG101");
-        bogies.add("BG102");
-        bogies.add("BG103");
-        bogies.add("BG104");
-        //Testin deuplicate ids
-        bogies.add("BG101");
-
-        System.out.println("Boggie id After insertion: ");
-        System.out.println(bogies);
+        List<String> trainconsist = new LinkedList<>();
+        trainconsist.add("Engine");
+        trainconsist.add("Sleeper");
+        trainconsist.add("AC");
+        trainconsist.add("Cargo");
+        trainconsist.add("Guard");
+        System.out.println("Initial train consist");
+        System.out.println(trainconsist);
+        trainconsist.add(2,"Pantry car");
+        System.out.println("After inserting pantry car at position 2: ");
+        System.out.println(trainconsist);
+        trainconsist.removeFirst();
+        trainconsist.removeLast();
+        System.out.println("After removing first and last boggie: ");
+        System.out.println(trainconsist);
     }
     public static void main(String[] args){
         TrainConsistManagementApp ob=new TrainConsistManagementApp();
