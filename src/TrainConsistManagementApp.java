af05ef1fb73918a1c1219441e5568f2c9697a29c
Arjun <arjunanand206@gmail.com>
2026-03-27 16:32:13 +0530

    Preserve Insertion Order of Bogies (LinkedHashSet)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,24 +1,19 @@
-import java.util.*;
+import java.util.*; //Hashset willbe in the order of what you input, Set means it will not be in the order
 class TrainConsistManagementApp {
     void task() {
         System.out.println("===============================");
-        System.out.println("UC4- Maintain Ordered Boggie Consist");
+        System.out.println("UC5- Preserve insertion order of boggies");
         System.out.println("================================");
-        List<String> trainconsist = new LinkedList<>();
-        trainconsist.add("Engine");
-        trainconsist.add("Sleeper");
-        trainconsist.add("AC");
-        trainconsist.add("Cargo");
-        trainconsist.add("Guard");
-        System.out.println("Initial train consist");
-        System.out.println(trainconsist);
-        trainconsist.add(2,"Pantry car");
-        System.out.println("After inserting pantry car at position 2: ");
-        System.out.println(trainconsist);
-        trainconsist.removeFirst();
-        trainconsist.removeLast();
-        System.out.println("After removing first and last boggie: ");
-        System.out.println(trainconsist);
+        Set<String> formation = new LinkedHashSet<>();
+        formation.add("Engine");
+        formation.add("Sleeper");
+        formation.add("Cargo");
+        formation.add("Guard");
+        System.out.println("Final train consist");
+        System.out.println(formation);
+        formation.add("Sleeper");
+        System.out.println("After trying to insert duplicate sleeper: ");
+        System.out.println(formation);
     }
     public static void main(String[] args){
         TrainConsistManagementApp ob=new TrainConsistManagementApp();
