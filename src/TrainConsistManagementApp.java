1795a9ef948b3c52b0b3880d1cea3b0159cab333
Arjun <arjunanand206@gmail.com>
2026-04-07 14:58:24 +0530

    UC7 Sorting by capacity(comparator)

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,17 +1,31 @@
-import java.util.*; //Hashset willbe in the order of what you input, Set means it will not be in the order
+import java.util.*;
 class TrainConsistManagementApp {
+    static class Bogie{
+        String name;
+        int capacity;
+        Bogie(String name, int capacity) {
+            this.name = name;
+            this.capacity = capacity;
+        }
+    }
     void task() {
         System.out.println("===============================");
-        System.out.println("UC6- Map bogie to capacity (hashmap)");
+        System.out.println("UC7- Sort Bogies by Capacity ( Comparator) ");
         System.out.println("================================");
-        Map<String,Integer> capacitymap = new HashMap<>();
-        capacitymap.put("First class",24);
-        capacitymap.put("Sleeper",72);
-        capacitymap.put("Cargo",120);
-        capacitymap.put("AC Chair",56);
-        System.out.println("Bogie capacity details: ");
-        for(Map.Entry<String,Integer> mp:capacitymap.entrySet()){
-            System.out.println(mp.getKey()+"->"+mp.getValue());
+        List<Bogie> bogies = new ArrayList<>();
+        bogies.add(new Bogie("Sleeper",72));
+        bogies.add(new Bogie("Ac chair ",56));
+        bogies.add(new Bogie("First class",24));
+        bogies.add(new Bogie("General",90));
+        System.out.println("Before Sorting by Capacity: ");
+        for(Bogie b : bogies){
+            System.out.println(b.name+"->"+b.capacity);
+        }
+        Collections.sort(bogies,Comparator.comparingInt(b->b.capacity)); // (Ascending) -> [b1.capacity -b2.capacity] if +ve b2 first, if -ve b1 first
+        System.out.println();
+        System.out.println("After Sorting by capacity: ");
+        for(Bogie b : bogies){
+            System.out.println(b.name+"->"+b.capacity);
         }
     }
     public static void main(String[] args){
